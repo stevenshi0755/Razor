0f59f3faa69e6a17769943ceeb5e9064688301dd
义庆 石 <szsyq123@163.com>
2017-08-29 17:17:29 +0800

    8.29-3 by steven

--- Razor/Controllers/HomeController.cs
+++ Razor/Controllers/HomeController.cs
@@ -29,11 +29,23 @@ namespace Razor.Controllers
         //page93
         public ActionResult DemoExpression()
         {
-            ViewBag.ProductCount = 1;
+            ViewBag.ProductCount = 2;
             ViewBag.ExpressShip = true;
             ViewBag.ApplyDiscount = false;
             ViewBag.Supplier = null;
             return View(myProduct);
+        }
+        public ActionResult DemoArray()
+        {
+            Product[] array =
+            //Product[]是数组
+            {
+                new Product{Name="Kayak",Price=275M},
+                new Product{Name="Lifejacket",Price=48.95M},
+                new Product{Name="Soccer ball",Price=19.5M},
+                new Product{Name="Corner flag",Price=34.95M}
+            };
+            return View(array);
         }
     }
 }
